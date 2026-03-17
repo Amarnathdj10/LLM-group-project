a5f4da1a8b27c076afb0df8cb9c815d08d342e88
Amarnathdj10 <amarnathdj56@gmail.com>
2026-03-18 00:34:57 +0530

    ble

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
 import json
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
